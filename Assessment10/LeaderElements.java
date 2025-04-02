89672e8f7f3af5cfb99cff51d9e72a39e6357e6a
Ramana Kumar Reddy <158750892+RamanaKumar030@users.noreply.github.com>
2025-04-02 16:19:59 +0530

    Update LeaderElements.java

--- Assessment10/LeaderElements.java
+++ Assessment10/LeaderElements.java
@@ -1,17 +1,19 @@
-public class LeaderElements {
-    public static void printLeaders(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            boolean isLeader = true;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] < arr[j]) {
-                    isLeader = false;
-                    break;
-                }
-            }
-            if (isLeader) {
-                System.out.print(arr[i] + " ");
+import java.util.ArrayList;
+public class SuperiorElements {
+    public static ArrayList<Integer> findLeaders(int[] arr) {
+        ArrayList<Integer> leaders = new ArrayList<>();
+        int maxFromRight = arr[arr.length - 1];
+        leaders.add(maxFromRight);
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > maxFromRight) {
+                leaders.add(arr[i]);
+                maxFromRight = arr[i];
             }
         }
+        return leaders;
+    }
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+        System.out.println(findLeaders(arr));
     }
 }
