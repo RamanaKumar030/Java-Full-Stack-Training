5d19f2afcd4b21d5b285734cc20d4b6401b001ac
Ramana Kumar Reddy <158750892+RamanaKumar030@users.noreply.github.com>
2025-04-02 16:26:37 +0530

    Create MooreVotingAlgorithm.java

---
+++ Assessment10/MooreVotingAlgorithm.java
@@ -0,0 +1,17 @@
+public class MooreVotingAlgorithm {
+    public static int majorityElement(int[] arr) {
+        int count = 0, candidate = -1;
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 4, 2, 4, 4, 2, 4, 4};
+        System.out.println("Majority element: " + majorityElement(arr));
+    }
+}
