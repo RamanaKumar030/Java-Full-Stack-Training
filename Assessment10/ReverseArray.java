806e051f2e26b8714975eb3f5649995158e75200
Ramana Kumar Reddy <158750892+RamanaKumar030@users.noreply.github.com>
2025-04-02 16:20:48 +0530

    Update ReverseArray.java

--- Assessment10/ReverseArray.java
+++ Assessment10/ReverseArray.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
 public class ReverseArray {
-    public static void reverseArray(int[] arr) {
+    public static void reverse(int[] arr) {
         int left = 0, right = arr.length - 1;
         while (left < right) {
             int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
+            arr[left++] = arr[right];
+            arr[right--] = temp;
         }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
